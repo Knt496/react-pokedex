53200810aac4d2d306c2e521e6c65339f242f8ee
Knt496 <korozco@pernix-solutions.com>
2023-07-17 17:07:18 -0600

    install and configure tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h2>Hola</h2>
+      <button className="btn btn-blue">Button</button>
+    </div>  
+  )
+}
+
+export default App
