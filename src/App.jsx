67f4fd60bfe3a7847c21792fa49073ce2914a6ea
Knt496 <korozco@pernix-solutions.com>
2023-07-17 18:11:47 -0600

    first example

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="container mx-auto">
+      <h1 className="text-4xl bg-brown-500 text-brown">Hola</h1>
       <h2>Hola</h2>
-      <button className="btn btn-blue">Button</button>
+      <button className="btn bg-red-500 text-white">Button</button>
     </div>  
   )
 }
