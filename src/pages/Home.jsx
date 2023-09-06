0e9dbd8a69fc4e3598f9ef8d1cccad3e0a2f5d65
Knt496 <korozco@pernix-solutions.com>
2023-09-06 12:35:57 -0600

    card example 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,45 @@
+import Card from "../components/Card";
+import { useState } from "react";
+
 const Home = () => {
+
+  const [pokemons, setPokemons] = useState(
+    [
+      {
+        name: "Bulbasur",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      },
+      {
+        name: "Charmander",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+      },
+      {
+        name: "Charmeleon",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+      },
+      {
+        name: "Squirtle",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+      }
+    ]
+
+  )
+
+  const fillList = () => {
+    let itemList=pokemons.map((pokemon,index)=>{
+      return <Card key={index} pokemon={pokemon}></Card>
+    })
+    return itemList;
+  };
+
+
   return (
     <div className="container">
-      <span className="">Home Page</span>
-    </div>  
+      <div className="flex justify-between flex-wrap">
+        {fillList()}
+      </div>
+
+    </div>
     
   )
 }
